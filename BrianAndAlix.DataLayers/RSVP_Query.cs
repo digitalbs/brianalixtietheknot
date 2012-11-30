588f581cdb3e9eb9325395244fd0fe65f9d5a453
Brian Schneider <schneid.brian@gmail.com>
2012-11-29 22:26:03 -0600

    Changes to Brian and Alix.com

---
+++ BrianAndAlix.DataLayers/RSVP_Query.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrianandAlix.DataLayers
+{
+    public class RSVP_Query
+    {
+        public static List<string> QueryGuests()
+        {
+            List<string> group = new List<string>();
+            using (var context = new BrianAlixDataModelContainer())
+            {
+                var people = from c in context.People
+                             where c.FirstName == "Brian"
+                             select
+                                 new { c.FirstName, c.LastName };
+               
+            }
+
+            return group;
+        }
+    }
+}
---
+++ BrianandAlix.web/AjaxWebMethods.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+namespace BrianandAlix.web
+{
+    public partial class AjaxWebMethods : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        [WebMethod]
+        public static string submitRSVP(string values)
+        {
+            string blah = string.Empty;
+            return blah;
+        }
+    }
+}
